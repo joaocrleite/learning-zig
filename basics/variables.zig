cee23d0befe4b9909596a3a87a13de9db1990434
joaocrleite <joaocrleite@gmail.com>
2023-09-17 13:11:45 -0300

    var vs const on re assign

--- basics/variables.zig
+++ basics/variables.zig
@@ -6,6 +6,8 @@ pub fn main() !void {
     var a: i32 = undefined;
     a = 50;
 
+    const c: i32 = 60;
+
     // const b = a;
     // OUTPUT:
     // a i32 50
@@ -25,13 +27,20 @@ pub fn main() !void {
     // signed 8-bit int cannot represent all possible signed 32-bit values
 
     const b: i8 = @as(i8, @intCast(a));
+    const d: i8 = c;
 
     try console
         .print("a {} {} \n", .{ @TypeOf(a), a });
     try console
         .print("b {} {} \n", .{ @TypeOf(b), b });
+    try console
+        .print("c {} {} \n", .{ @TypeOf(c), c });
+    try console
+        .print("d {} {} \n", .{ @TypeOf(d), d });
 }
 
 // zig run ./basics/variables.zig
 // a i32 50
 // b i8 50
+// c i32 60
+// d i8 60
