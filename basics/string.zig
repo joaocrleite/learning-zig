9ae82f1b86ad315b572e035b1a0a01bf87c4d832
joaocrleite <joaocrleite@gmail.com>
2023-09-17 13:40:01 -0300

    upper case letter and lowercase letter

--- basics/string.zig
+++ basics/string.zig
@@ -17,11 +17,17 @@ pub fn main() !void {
     // char ascii table 65 equals letter A
     const b: u8 = 65;
 
-    // char ascii table 99 equals letter c
-    const c: u8 = 'c';
+    // char ascii table 67 equals letter c
+    const c: u8 = 'C';
 
     // char ascii table 100 equals letter d
     const d: u8 = 'd';
+
+    // char ascii table 97 - 32 = 65(A)
+    const aUp = a - 32;
+
+    // char ascii table 67 + 32 = 99(c)
+    const cLow = c + 32;
 
     const w: [4]u8 = [4]u8{ a, b, c, d };
     const wl = w.len;
@@ -30,10 +36,16 @@ pub fn main() !void {
         .print("const a type:{} ascii:{d} letter:{c} \n", .{ @TypeOf(a), a, a });
 
     try console
+        .print("const aUp type:{} ascii:{d} letter:{c} \n", .{ @TypeOf(aUp), aUp, aUp });
+
+    try console
         .print("const b type:{} ascii:{d} letter:{c} \n", .{ @TypeOf(b), b, b });
 
     try console
         .print("const c type:{} ascii:{d} letter:{c} \n", .{ @TypeOf(c), c, c });
+
+    try console
+        .print("const cLow type:{} ascii:{d} letter:{c} \n", .{ @TypeOf(cLow), cLow, cLow });
 
     try console
         .print("const d type:{} ascii:{d} letter:{c} \n", .{ @TypeOf(d), d, d });
@@ -44,7 +56,9 @@ pub fn main() !void {
 
 // zig run ./basics/string.zig
 // const a type:u8 ascii:97 letter:a
+// const aUp type:u8 ascii:65 letter:A
 // const b type:u8 ascii:65 letter:A
-// const c type:u8 ascii:99 letter:c
+// const c type:u8 ascii:67 letter:C
+// const cLow type:u8 ascii:99 letter:c
 // const d type:u8 ascii:100 letter:d
-// const w type:[4]u8 len:4 word:aAcd
+// const w type:[4]u8 len:4 word:aACd
