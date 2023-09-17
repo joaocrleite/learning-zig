d4d4a05ad8231a436f883d446d8ba76c8f56d361
joaocrleite <joaocrleite@gmail.com>
2023-09-17 13:09:58 -0300

    working with var the re assign must use castInt

--- basics/variables.zig
+++ basics/variables.zig
@@ -3,20 +3,35 @@ const std = @import("std");
 const console = std.io.getStdOut().writer();
 
 pub fn main() !void {
-    const const_number: i32 = 50; // signed 32-bit constant
-    const const_number_clone: i8 = const_number; // signed 8-bit constant, if the number can't fit 8-bit then an error will occurs
-    const const_number_force_type = @as(i8, const_number); // @as performs an explicit type coercion
-    try console
-        .print("const_number {} {} \n", .{ @TypeOf(const_number), const_number });
+    var a: i32 = undefined;
+    a = 50;
+
+    // const b = a;
+    // OUTPUT:
+    // a i32 50
+    // b i32 50
+
+    // const b: i8 = a;
+    // OUTPUT ERROR:
+    // signed 8-bit int cannot represent all possible signed 32-bit values
+
+    // const b: i8 = @intCast(a);
+    // OUTPUT:
+    // a i32 50
+    // b i8 50
+
+    // const b: i8 = @as(i8, a);
+    // OUTPUT ERROR:
+    // signed 8-bit int cannot represent all possible signed 32-bit values
+
+    const b: i8 = @as(i8, @intCast(a));
 
     try console
-        .print("const_number_clone {} {} \n", .{ @TypeOf(const_number_clone), const_number_clone });
-
+        .print("a {} {} \n", .{ @TypeOf(a), a });
     try console
-        .print("const_number_force_type {} {} \n", .{ @TypeOf(const_number_force_type), const_number_force_type });
+        .print("b {} {} \n", .{ @TypeOf(b), b });
 }
 
 // zig run ./basics/variables.zig
-// const_number i32 50
-// const_number_clone i8 50
-// const_number_force_type i8 50
+// a i32 50
+// b i8 50
