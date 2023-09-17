d549bbf954f7a259a34d6e4c7f622e892b997874
joaocrleite <joaocrleite@gmail.com>
2023-09-17 15:06:31 -0300

    simple and custom multiples

--- exercises/project-euler/euler1.zig
+++ exercises/project-euler/euler1.zig
@@ -4,6 +4,17 @@ const expect = @import("std").testing.expect;
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5,6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
+fn find_sum(below: usize) usize {
+    var sum: usize = 0;
+    var i: usize = 1;
+    while (i < below) : (i += 1) {
+        if (i % 3 == 0 or i % 5 == 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
 fn find_sum_of_multiples(below: usize, multiples: []u8) usize {
     var sum: usize = 0;
     var i: usize = 1;
@@ -25,13 +36,23 @@ fn find_sum_of_multiples(below: usize, multiples: []u8) usize {
 }
 
 test "sum of multiples of 3 or 5 below 10" {
+    const result = find_sum(10);
+    try expect(result == 23);
+}
+
+test "sum of multiples of 3 or 5 below 1000" {
+    const result = find_sum(1_000);
+    try expect(result == 233_168);
+}
+
+test "sum of multiples below 10" {
     const below: usize = 10;
     var multiples = [2]u8{ 3, 5 };
     const sum = find_sum_of_multiples(below, &multiples);
     try expect(sum == 23);
 }
 
-test "sum of multiples of 3 or 5 below 1000" {
+test "sum of multiples below 1000" {
     const below: usize = 1_000;
     var multiples = [2]u8{ 3, 5 };
     const sum = find_sum_of_multiples(below, &multiples);
