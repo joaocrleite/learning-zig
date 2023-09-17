b04d3f2e612ba1b715dd3a36662cd8d6076da853
joaocrleite <joaocrleite@gmail.com>
2023-09-17 12:52:11 -0300

    explicit type coercion

--- basics/variables.zig
+++ basics/variables.zig
@@ -3,12 +3,16 @@ const std = @import("std");
 const console = std.io.getStdOut().writer();
 
 pub fn main() !void {
-    const const_number: i32 = 5; // signed 32-bit constant
-    var var_number: u32 = 5000; // unsigned 32-bit variable
+    const const_number: i32 = 50; // signed 32-bit constant
+    const const_number_clone: i8 = const_number; // signed 8-bit constant, if the number can't fit 8-bit then an error will occurs
 
     try console
-        .print("{}, {} \n", .{ const_number, var_number });
+        .print("const_number {} {} \n", .{ @TypeOf(const_number), const_number });
+
+    try console
+        .print("const_number_clone {} {} \n", .{ @TypeOf(const_number_clone), const_number_clone });
 }
 
 // zig run ./basics/variables.zig
-// 5, 5000
+// const_number i32 50
+// const_number_clone i8 50
